1a99e388e499158b17ecf6a3592241c887b3b47c
Antun Šafarić <crosafan@gmail.com>
2020-04-10 13:15:12 +0100

    Added tooltips, and coins table

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,39 @@ function calculateMinionCoins() {
   upgradeSlot1 = document.getElementById("upgradeSlot1").value;
   upgradeSlot2 = document.getElementById("upgradeSlot2").value;
   bonusSpeed = parseFloat(document.getElementById("bonusSpeed").value) / 100;
+  usingDiaSpreading = false;
+  totalBonuses = 0.0
+
   if (upgradeSlot1 === "dia-spreading") {
-    upgradeSlot2 = parseFloat(upgradeSlot2) / 100
-    coinsProducedPerMinion =
-      88400 / (actionTime / (1 + fuel + bonusSpeed + upgradeSlot2)) * itemsPerAction * unitPrice + 138240 / actionTime;
+    upgradeSlot2 = parseFloat(upgradeSlot2) / 100;
+    totalBonuses = fuel + bonusSpeed + upgradeSlot2;
+    usingDiaSpreading = true;
+
   } else if (upgradeSlot2 === "dia-spreading") {
-    upgradeSlot1 = parseFloat(upgradeSlot1) / 100
-    coinsProducedPerMinion =
-      88400 / (actionTime / (1 + fuel + bonusSpeed + upgradeSlot1)) * itemsPerAction * unitPrice + 138240 / actionTime;
+    upgradeSlot1 = parseFloat(upgradeSlot1) / 100;
+    totalBonuses = fuel + bonusSpeed + upgradeSlot1
+    usingDiaSpreading = true
+
+  } else {
+    upgradeSlot1 = parseFloat(upgradeSlot1) / 100;
+    upgradeSlot2 = parseFloat(upgradeSlot2) / 100;
+
+    totalBonuses = fuel + bonusSpeed + upgradeSlot1 + upgradeSlot2;
   }
+  if (usingDiaSpreading) {
+    coinsProducedPerMinion = 88400 / (actionTime / (1 + totalBonuses)) * itemsPerAction * unitPrice + 138240 / actionTime;
 
-
-
-  else {
-    upgradeSlot1 = parseFloat(upgradeSlot1) / 100
-    upgradeSlot2 = parseFloat(upgradeSlot2) / 100
-
-    coinsProducedPerMinion = 88400 / (actionTime / (1 + fuel + bonusSpeed + upgradeSlot1 + upgradeSlot2)) * itemsPerAction * unitPrice;
+  } else {
+    coinsProducedPerMinion = 88400 / (actionTime / (1 + totalBonuses)) * itemsPerAction * unitPrice;
   }
 
   totalCoins = coinsProducedPerMinion * minionSlots;
 
   //TODO : fix rounding errors
-  document.getElementById("result").innerText = "Coins with " + minionSlots + " minion slots is:" + totalCoins
+  document.getElementById("oneHourCoins").innerText = totalCoins / 24
+  document.getElementById("oneDayCoins").innerText = totalCoins
+  document.getElementById("oneWeekCoins").innerText = totalCoins * 7
+
+
 
 }
